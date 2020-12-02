78f4c5b0e68da7c2cefd7d16cc02f4f65794767d
Rafael Ventura <raf4ael@gmail.com>
2020-12-01 22:03:03 -0300

    adding new modif

--- basics/arrays2.js
+++ basics/arrays2.js
@@ -1,4 +1,4 @@
-const notes = [{},{
+const notes = [{
     title: 'latin america',
     body: 'corpao violao'
 }, {
@@ -10,16 +10,24 @@ const notes = [{},{
 }
 ]
 
-// Para procurar o indice de algum valor num array de objetos(principalmente),
-// eh recomendado usar o finIndex(function (item, index)) e procurar, manualmente, a propriedade procurada
+// // Para procurar o indice de algum valor num array de objetos(principalmente),
+// // eh recomendado usar o finIndex(function (item, index)) e procurar, manualmente, a propriedade procurada
+// // metodo que retorna um valor inteiro representando o index. 
+// // se for -1, nao esta presente no array
 
-// metodo que retorna um valor inteiro representando o index. 
-// se for -1, nao esta presente no array
-const posicao = notes.findIndex(function (item, index){
-    console.log(item);
-    return item.title === 'africa'
-    //return item.title === 'Habbits to work on' //retornar -1
+// const posicao = notes.findIndex(function (item, index){
+//     console.log(item);
+//     return item.title === 'africa'
+//     //return item.title === 'Habbits to work on' //retornar -1
 
-})
+// })
+// console.log(posicao);
 
-console.log(posicao);
+const findNoteByTitle = function (notesFile, noteWanted) {
+    return notes.find(function (item, index) {
+        return item.title.toUpperCase() === noteWanted.toUpperCase()
+    })
+}
+
+const note = findNoteByTitle(notes, 'africa');
+console.log(note);
---
+++ todo.js
@@ -0,0 +1,24 @@
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean Ktichen',
+    completed: false
+}, {
+    text: 'Buy Food',
+    completed: false
+}]
+const deleteTodo = function (todos, todoText){
+    const indexToDo = todos.findIndex(function(item) {
+       return item.text.toUpperCase() === todoText.toUpperCase()
+    })
+    if (indexToDo > -1) {
+        todos.splice(indexToDo,1);
+    }
+}
+console.log(deleteTodo(todos, 'Buy Foods'));
+console.log(todos);
+
+
+// convert array to array of objects -> text, completed
+// create function to remove a todo by text value
